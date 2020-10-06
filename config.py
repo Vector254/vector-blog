7052b928b6523c477c5d35185a90ec1f8dfe6aa9
Vector254 <ochrist7@gmail.com>
2020-10-06 07:27:41 +0300

    configure for deployment

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
 
 class ProdConfig(Config):
   
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     '''
     Production  configuration child class
     Args:
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager, Server
 from app import create_app, db
-from app.models import User, Posts, Comment
+from app.models import User, Posts, Comment, Quote
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -15,7 +15,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User=User, Posts=Posts,Comment=Comment)
+    return dict(app = app,db = db,User=User, Posts=Posts,Comment=Comment, Quote=Quote)
 
 if __name__ == "__main__":
     manager.run()
